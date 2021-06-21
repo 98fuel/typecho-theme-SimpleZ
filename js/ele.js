7f3307065e8426c8212e321846001a815284a25b
shuxhan <shuxhan@163.com>
2021-06-21 14:38:57 +0800

    init

---
+++ js/ele.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+})
