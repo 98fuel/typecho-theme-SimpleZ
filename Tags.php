4b85c5d87c2ae72e47a87dcb3c47e4c78163e880
shuxhan <shuxhan@163.com>
2021-07-21 16:04:06 +0800

    优化ui

--- Tags.php
+++ Tags.php
@@ -9,21 +9,26 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
+
 <div class="main">
-
-    <div class="post">
-        <div class="title-page">
-            <h3>标签云</h3>
-            <p>保持分类的好习惯，让我们更方便的管理和处理数据</p>
+    
+        <div class="post">
+                <div class="title-page">
+                    <h3>标签云</h3>
+                    <em>保持分类的好习惯，让我们更方便的管理和处理数据</em>
+                    <hr>
+                    
+                </div>
+                <div class="tags-content">
+                    <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=30')->to($tags); ?>
+                    <?php while($tags->next()): ?>
+                        <a class="layui-btn layui-btn-xs layui-btn-primary" href="<?php $tags->permalink(); ?>" title='<?php $tags->name(); ?>'><?php $tags->name(); ?></a>
+                    <?php endwhile; ?>
+                </div>
+            
         </div>
-        <div class="tags-content">
-            <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=30')->to($tags); ?>
-            <?php while ($tags->next()) : ?>
-                <a href="<?php $tags->permalink(); ?>" title='<?php $tags->name(); ?>'><?php $tags->name(); ?></a>
-            <?php endwhile; ?>
-        </div>
-    </div>
-
+        
+        
 </div>
 
 <?php $this->need('footer.php'); ?>
