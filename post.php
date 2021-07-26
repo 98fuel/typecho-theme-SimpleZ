5c8aa2339bf00e0ca32bd4a160a14113bfa36b78
shuxhan <shuxhan@163.com>
2021-07-26 14:33:42 +0800

    Update post.php

--- post.php
+++ post.php
@@ -14,9 +14,9 @@
                 
                 <div class="title-msg">
                     <span><?php $this->date('Y-m-d'); ?> </span>
-                    <span>· <?php $this->Category(', ', true, 'none'); ?></span>~
-                    <span>本文共 <?php echo art_count($this->cid); ?> 字</span>~
-                    <span>阅读: <?php echo ViewsCounter_Plugin::getViews(); ?></span>
+                    <span>· <?php $this->Category(', ', true, 'none'); ?></span>
+                    <span>· 本文共 <?php echo art_count($this->cid); ?> 字</span>
+                    <span>· 阅读: <?php echo ViewsCounter_Plugin::getViews(); ?></span>
                 </div>
             </div>
                 
@@ -27,12 +27,12 @@
                 
             
             <!--版权信息-->
-            <details class="copy-text">
+            <details class="copy-text" open>
                 <summary>版权声明</summary>
                 <ul>
                     <li>若无特殊说明，本站所有文章均为博主原创，如若转载，注明出处与链接即可。</li>
-                    <li>本文最后更新时间为&nbsp;&nbsp;<span style="color:#000;text-decoration:underline;"><?php echo date('Y-m-d H:i:s' , $this->modified); ?></span>&nbsp;&nbsp;(<span class="lately-c"><?php echo date('Y-m-d H:i:s' , $this->modified); ?></span>)，如果资源无效请在评论区留言。</li>
-                    <li>本文首次发布：<a href="<?php $this->permalink() ?>"><?php $this->permalink() ?></a></li>
+                    <li>最后更新时间为&nbsp;&nbsp;<span style="color:#000;text-decoration:underline;"><?php echo date('Y-m-d H:i:s' , $this->modified); ?></span>&nbsp;&nbsp;(<span class="lately-c"><?php echo date('Y-m-d H:i:s' , $this->modified); ?></span>)，如果资源无效请在评论区留言。</li>
+                    <li>文章链接：<a href="<?php $this->permalink() ?>"><?php $this->permalink() ?></a></li>
                 </ul>
             </details>
            
